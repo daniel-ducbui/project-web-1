efe4c97a82cce82fb19016bc4e0175b30a604495
Daniel <44046364+Danieldot29@users.noreply.github.com>
2019-11-30 00:32:57 +0700

    Add post/delete status functions

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\PostsFormRequest;
+use App\Post;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Auth\Events\Login;
@@ -25,6 +27,75 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $user = Auth::user();
+
+        $posts = Post::orderBy('created_at', 'desc')->paginate(10);
+
+        return view('home', compact('posts'))->with(['user' => $user]);
+    }
+
+    public function store(PostsFormRequest $request)
+    {
+        $user = Auth::user();
+
+        $post = new Post();
+
+        $noti = 'Something wrong here! Try again later!';
+
+        if (!$request->file('post_photo') && !$request->get('post_content')) {
+            $noti = 'Post what?';
+        } else {
+            $post->privacy = $request['privacy'];
+
+            if ($request['post_content']) {
+                $post->post_content = $request['post_content'];
+            }
+
+            //// Upload image
+            if ($request->file('post_photo')) {
+                // Get file
+                $image = $request->file('post_photo');
+                // Directory
+                $imagePath = 'src/images/post/';
+                // Get file name
+                $imageName = $image->getClientOriginalName();
+
+                $imageName = '_temp.jpg';
+                // Store file
+                $image->move(public_path($imagePath), $imageName);
+                // Resize image --> Pending
+
+
+                // Convert to string
+                $imageTmp = file_get_contents($imagePath . $imageName);
+                // Save data to database
+                $post->post_photo = $imageTmp;
+            }
+
+            if ($request->user()->posts()->save($post)) {
+                $noti = 'Status posted';
+            }
+        }
+
+        return response()->redirectToRoute('home')->with(['message' => $noti]);
+    }
+
+    public function destroy($post_id)
+    {
+        $post = Post::where('id', $post_id)->first();
+
+        if (Auth::user() != $post->user) {
+            return redirect()->back()->with(['message' => 'Can not delete!']);
+        }
+        // Else
+        $post->delete();
+
+        return redirect()->back()->with(['message' => 'Post deleted']);
+    }
+
+    public function edit()
+    {
+        // Pending
+        return redirect()->back()->with('message', 'Function is not done yet!');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,29 @@ Auth::routes(['verify' => true]);
 
 Route::get('profile', function () {
     // Only verified users may enter...
+
 })->middleware('verified');
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+/// HomeController
+///
+Route::get('/home', 'HomeController@index')->name('home')->middleware('auth');
+
+Route::post('/home', 'HomeController@store')->name('post.store')->middleware('auth');
+
+Route::get('/home/{post_id}', 'HomeController@destroy')->name('post.delete');
+
+Route::post('', 'HomeController@edit')->name('post.edit'); // Pending
+///
+/// End HomeController
+///
+/// UsersController
+///
+Route::get('/profile/{user_name}/{user_id}', 'UsersController@userProfile')->name('user.profile')->middleware('auth');
+
+Route::get('/profile-details', 'UsersController@userInformation')->name('user.information')->middleware('auth');
+
+Route::post('/update-profile-details', 'UsersController@update')->name('user.update')->middleware('auth');
+///
+/// End UsersController
